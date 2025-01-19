eb5feafe56e4af06c7c4777715ac6bbd8cd2f185
tvpanchal0 <128672578+tvpanchal0@users.noreply.github.com>
2025-01-19 16:37:54 +0530

    issue in vercel

--- config/database/sql.js
+++ config/database/sql.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PASSWORD, {
     host: process.env.SQL_HOST,
-    dialect: 'mysql', // Change to 'postgres' for PostgreSQL
+    dialectModule: require('mysql2')
 });
 
 sequelize.authenticate()
