b645bebbef7500cfbabcf81627a3dcbe95a6d52d
tvpanchal0 <128672578+tvpanchal0@users.noreply.github.com>
2025-01-19 16:45:56 +0530

    Update sql.js

--- config/database/sql.js
+++ config/database/sql.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PASSWORD, {
     host: process.env.SQL_HOST,
-    dialectModule: 'mysql'
+    dialect: 'mysql',
+    dialectModule: require('mysql2')
 });
 
 sequelize.authenticate()
